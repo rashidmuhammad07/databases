42558c9ba3b04c0da99bf3e297fd59b5b5772316
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-11-03 19:59:54 -0700

    Started to implement router functionality and schemas

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,42 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // get the data from the database
+      const data = 'This (message) data needs to come from the database'; // fill this with a get request to the database
+
+      // return the data to the user
+      res.send(data);
+      
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      // extract the data from the POST request        
+      const {id, message, username, roomname} = req.body;
+
+      // send the data to the database
+      // TODO
+            
+      // send a response, so that it knows we recieved the data
+      // NOTE: should set a header and replace the below with something more succinct
+      res.end(`You sent this data: \n${id} \n${message} \n${username} \n${roomname}`);
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      const data = 'This (user) data needs to come from the database';
+    
+      res.send(data);
+    },
+    post: function (req, res) {
+      const {id, username} = req.body;
+    
+      // send the data to the database
+      // TODO
+
+      res.end(`You sent this data: \n${id} \n${username}`);
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,24 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  username TEXT,
+  message TEXT,
+  roomname TEXT
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username TEXT
+);
 
+-- CREATE TABLE messages_users (
+--   id INTEGER PRIMARY KEY,
+--   user_id INTEGER,
+--   message_id INTEGER
+-- );
 
 
 /*  Execute this file from the command line by typing:
